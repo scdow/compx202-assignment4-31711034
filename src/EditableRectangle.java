a6bae579591afb782d9d461db24ce49860505b32
scdow <920301714@qq.com>
2020-04-20 10:04:54 +0800

    Step 1-4: Second EditableTriangle, EditableRectangle, top left anchor of rectangle to move the whole rectangle

---
+++ src/EditableRectangle.java
@@ -0,0 +1,46 @@
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+
+class EditableRectangle extends Group {
+
+    private Rectangle rectangle;
+//    private Anchor[] anchors;
+    private Anchor anchor_topleft;
+
+    public EditableRectangle(double x,double y,double width,double height){
+        rectangle = new Rectangle(x, y, width,height);
+        rectangle.setFill(Color.LIGHTBLUE);
+        rectangle.setStroke(Color.FORESTGREEN);
+        rectangle.setStrokeWidth(10);
+        rectangle.setStrokeType(StrokeType.CENTERED);
+        getChildren().add(rectangle);
+
+        anchor_topleft = new Anchor(rectangle.getX(),rectangle.getY());
+        anchor_topleft.addListener(obs ->{ //obs: myObserverable
+            Anchor an = (Anchor) obs;
+            rectangle.setX(anchor_topleft.getCenterX());
+            rectangle.setY(anchor_topleft.getCenterY());
+        });
+        getChildren().add(anchor_topleft);
+
+//        anchors = new Anchor[3];
+//        for (int i = 0; i < 3; i++) {
+//            final int ifinal = i;
+//            double x = points.get(2 * i);
+//            double y = points.get(2 * i + 1);
+//            anchors[i] = new Anchor(x, y);
+//            // When the anchor changes, update the Polygon's corresponding point
+//            anchors[i].addListener(obs -> {
+//                Anchor an = (Anchor) obs;
+//                triangle.getPoints().set(2 * ifinal, an.getCenterX());
+//                triangle.getPoints().set(2 * ifinal + 1, an.getCenterY());
+//            });
+//            getChildren().add(anchors[i]);
+//        }
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.Scene;
+import javafx.scene.shape.FillRule;
+import javafx.scene.shape.StrokeLineJoin;
+import javafx.scene.shape.StrokeType;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
 
 
 public class Main extends Application {
@@ -14,7 +20,12 @@ public class Main extends Application {
     public void start(final Stage stage) {
         Group root = new Group();
         EditableTriangle triangle = new EditableTriangle(100.0, 100.0, 150.0, 50.0, 250.0, 150.0);
+        EditableTriangle triangle2 = new EditableTriangle(200,400,150,200,300,300);
         root.getChildren().add(triangle);
+        root.getChildren().add(triangle2);
+
+        EditableRectangle rectangle = new EditableRectangle(400,200,130,100);
+        root.getChildren().add(rectangle);
 
         Scene scene = new Scene(root, 600, 500, Color.BISQUE);
         stage.setTitle("Assignment 4");
